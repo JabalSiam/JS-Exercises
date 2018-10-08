816d57395a402e0d087a1c4613171560d80c01d2
Jabal Siam <1521339@iub.edu.bd>
2018-10-08 03:07:22 +0000

    task 4

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function sumArray(arr)
 }
 
 console.log(sumArray([2,4,5,6,7]));
+//Task-3
 
 function checkEmail(emailString){
     var emailFormat= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
@@ -25,4 +26,31 @@ function checkEmail(emailString){
 }
 console.log(checkEmail("john@Smith.com"));
 console.log(checkEmail("johnsmith.com"));
+
+//Task 4
+
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+   
+   function getReadingStatus(index){
+       var book=library[index];
+       return book.readingStatus;
+   }
+   console.log(getReadingStatus(1));
+   console.log(getReadingStatus(0));
+   console.log(getReadingStatus(2));
    
